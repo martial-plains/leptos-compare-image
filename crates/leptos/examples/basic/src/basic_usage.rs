01a9ee4a0d8e145d3eb5befa98509d98985f5b16
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-09-24 18:15:21 -0400

    added basic examples

---
+++ crates/leptos/examples/basic/src/basic_usage.rs
@@ -0,0 +1,23 @@
+use leptos::*;
+use leptos_compare_image::LeptosCompareImage;
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    let img1_src = "images/image1.png";
+    let img2_src = "images/image2.png";
+
+    mount_to_body(move || {
+        view! {
+            <div style="max-width: 320px;">
+                "Horizontal comparison"
+                <LeptosCompareImage left_image=img1_src right_image=img2_src />
+
+                <br />
+
+                "Vertical comparison"
+                <LeptosCompareImage left_image=img1_src right_image=img2_src vertical=true />
+            </div>
+        }
+    })
+}
